39e08205a637a6ae80e3e74d9d4b10b82f31a6f2
val-h <val_dev@zohomail.eu>
2021-05-21 05:17:56 +0100

    inbox and email view nearly finished, first actual progress on this project

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -7,7 +7,7 @@ document.addEventListener('DOMContentLoaded', function() {
   document.querySelector('#compose').addEventListener('click', compose_email);
 
   document.querySelector('#compose-form').onsubmit = send_email;
-
+  
   // By default, load the inbox
   load_mailbox('inbox');
 });
@@ -16,6 +16,7 @@ function compose_email() {
 
   // Show compose view and hide other views
   document.querySelector('#emails-view').style.display = 'none';
+  document.querySelector('#detail-view').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'block';
 
   // Clear out composition fields
@@ -28,10 +29,13 @@ function load_mailbox(mailbox) {
   
   // Show the mailbox and hide other views
   document.querySelector('#emails-view').style.display = 'block';
+  document.querySelector('#detail-view').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'none';
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+  load_mail(mailbox)
+
 }
 
 function send_email() {
@@ -51,4 +55,63 @@ function send_email() {
   .then(result => {
     console.log(result);
   })
+  .catch(e => {
+    console.log(e);
+  })
+}
+
+function load_mail(mailbox) {
+  fetch(`/emails/${mailbox}`)
+  .then(response => response.json())
+  .then(emails => {
+    console.log(emails);
+
+    emails_view = document.querySelector('#emails-view');
+    for (i = 0; i < emails.length; i++) {
+      const crnt_email = emails[i]
+      // Create the container that will hold the email
+      mail_container = document.createElement('DIV');
+      // mail_container.innerHTML = emails[i].subject;
+      mail_container.className = 'mail';
+      mail_container.addEventListener('click', () => detail_view(crnt_email));
+
+      // Create the subject section
+      mail_subject = document.createElement('DIV');
+      mail_subject.innerHTML = crnt_email.subject + crnt_email.id;
+      mail_subject.className = 'mail-subject';
+
+      // Add a date field
+      mail_date = document.createElement('div');
+      mail_date.innerHTML = crnt_email.timestamp;
+      mail_date.className = 'mail-date';
+
+      //  Structure the divs
+      mail_container.appendChild(mail_subject);
+      mail_container.appendChild(mail_date);
+
+      // Check if the email is read
+      if (crnt_email.read == true) {
+        mail_container.style.background = 'lightgray';
+      }
+
+      // Finally appoend the container to the email view
+      emails_view.appendChild(mail_container);
+    }
+  });
+}
+
+function detail_view(email) {
+  console.log(email)
+  document.querySelector('#emails-view').style.display = 'none';
+  document.querySelector('#detail-view').style.display = 'block';
+  document.querySelector('#compose-view').style.display = 'none';
+
+  document.querySelector('#email-from').innerHTML = email.sender;
+  document.querySelector('#email-to').innerHTML = email.recepients;
+  document.querySelector('#email-subject').innerHTML = email.subject;
+  document.querySelector('#email-timestamp').innerHTML = email.timestamp;
+
+  // TODO - add a reply button, put request to /emails/<int:id> for being read
+
+  document.querySelector('#email-body').innerHTML = email.body;
 }
